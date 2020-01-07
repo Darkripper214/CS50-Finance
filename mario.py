86401f49ad0ef0e7cdc60790340610bc17b86c39
Darkripper214 <Darkripper214@users.noreply.github.com>
2020-01-07 22:38:33 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,10 +1,12 @@
 def print_hash(x):
-    for i in range(1,x+1):
+    for i in range(1, x+1):
         print("#", end="")
 
+
 def print_space(y):
-    for i in range(1,y+1):
+    for i in range(1, y+1):
         print(" ", end="")
+
 
 def get_positive_int():
     i = 0
@@ -13,17 +15,16 @@ def get_positive_int():
     else:
         return i
 
+
 height = get_positive_int()
-spaces = height -1
+spaces = height - 1
 hashes = 1
 
-for x in range(1,height+1):
+for x in range(1, height+1):
     print_space(spaces)
     print_hash(hashes)
-    print(" ",end="")
+    print("  ", end="")
     print_hash(hashes)
     print("")
     spaces -= 1
     hashes += 1
-
-
