242516662ac364c295beaaff49abbf17dc40bcc5
Darkripper214 <Darkripper214@users.noreply.github.com>
2020-01-07 22:48:37 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,5 +1,6 @@
 from cs50 import get_int
 
+
 def print_hash(x):
     for i in range(1, x+1):
         print("#", end="")
@@ -14,7 +15,8 @@ def get_positive_int():
     i = 0
     while True:
         i = get_int("Height: ")
-        return (i)
+        if i >= 1 and i < 8:
+            return i
 
 
 height = get_positive_int()
