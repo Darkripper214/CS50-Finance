fba0117468b29dc1d4266256c86ecc45db01cc53
Darkripper214 <Darkripper214@users.noreply.github.com>
2020-01-13 17:12:35 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -19,24 +19,23 @@ for row in DBreader:
 
 str_list.pop(0)
 
-seq_dict ={}
-seq_dict = dict.fromkeys(str_list,0)
-#print(seq_dict)
+seq_dict = {}
+seq_dict = dict.fromkeys(str_list, 0)
+# print(seq_dict)
 
 def equal_count():
     return cur_count
 
 
-
 SQ = open(seqPath, "r")
 for line in SQ:
-    #Looping through each STR type loaded
+    # Looping through each STR type loaded
     for y in range(len(str_list)):
         cur_count = 0
         i = 0
         x = len(line)
         while i <= x:
-            #Check if DNA STR match, increase the string by i + len(STR) if match
+            # Check if DNA STR match, increase the string by i + len(STR) if match
             if line[i:i+len(str_list[y])] == str_list[y]:
                 cur_count += 1
                 i += len(str_list[y]) - 1
@@ -45,11 +44,11 @@ for line in SQ:
                     seq_dict[str_list[y]] = cur_count
 
                 cur_count = 0
-            #Increase i by 1 if doesn't match
+            # Increase i by 1 if doesn't match
             i += 1
 
 
-#print(seq_dict)
+# print(seq_dict)
 match = 0
 DB = open(dbPath, 'r')
 DBreader = csv.DictReader(DB)
