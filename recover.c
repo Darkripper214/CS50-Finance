f1e8e21fc86f34532f7fe1028179c99c1be0d23e
Darkripper214 <Darkripper214@users.noreply.github.com>
2020-02-10 16:57:47 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -12,39 +12,39 @@ bool find_JPEG(BYTE[]);
 FILE* newfile(FILE*, char[], int, BYTE[]);
 
 int main(int argc, char* argv[])
- {
+{
     // check correct usage of program
-    if(argc != 2)
+    if (argc != 2)
     {
-    fprintf(stderr, "Error!: must be in format ./recover xxx.raw");
-    return 1;
-}
+        fprintf(stderr, "Error!: must be in format ./recover xxx.raw");
+        return 1;
+    }
 
-// open file
-char* input = argv[1];
-FILE* infile = fopen(input, "r");
-
-
-// check file is in right format
-if (infile == NULL)
-{
-    fprintf(stderr, "Could not open %s.\n", input);
-    return 2;
-}
-
-BYTE buffer[BUFSIZE];
-int JPEG_counter = 0;
-char filename [8];
-FILE* img = NULL;
+    // open file
+    char* input = argv[1];
+    FILE* infile = fopen(input, "r");
 
 
-while(!feof(infile) && fread(&buffer, BUFSIZE, 1, infile) == true)
-{
-    //  start if image is there
-    if(find_JPEG(buffer) == true)
+    // check file is in right format
+    if (infile == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", input);
+        return 2;
+    }
+
+    BYTE buffer[BUFSIZE];
+    int JPEG_counter = 0;
+    char filename [8];
+    FILE* img = NULL;
+
+
+    while (!feof(infile) && fread(&buffer, BUFSIZE, 1, infile) == true)
+    {
+        //  start if image is there
+        if (find_JPEG(buffer) == true)
         {
             // special case for first image
-            if(JPEG_counter == 0)
+            if (JPEG_counter == 0)
             {
                 // start new file
                 sprintf(filename, "%03i.jpg", JPEG_counter);
@@ -55,10 +55,10 @@ while(!feof(infile) && fread(&buffer, BUFSIZE, 1, infile) == true)
             // if not first image
             else
             {
-               // close current file
-               fclose(img);
+                // close current file
+                fclose(img);
 
-               // start new file
+                // start new file
                 sprintf(filename, "%03i.jpg", JPEG_counter);
                 img = fopen(filename, "w");
                 fwrite(buffer, 1, BUFSIZE, img);
@@ -66,38 +66,35 @@ while(!feof(infile) && fread(&buffer, BUFSIZE, 1, infile) == true)
             }
         }
 
-    // if there are no new jpeg
-    if(find_JPEG(buffer) == false)
-    {
-        // continue to next buffer
-        if(JPEG_counter == 0)
+        // if there are no new jpeg
+        if (find_JPEG(buffer) == false)
         {
-            continue;
-        }
-        else
-        // else write to file
-        {
-            fwrite(buffer, 1, BUFSIZE, img);
+            // continue to next buffer
+            if (JPEG_counter == 0)
+            {
+                continue;
+            }
+            else
+               // else write to file
+            {
+                fwrite(buffer, 1, BUFSIZE, img);
+            }
         }
     }
-}
 
-if(feof(infile))
-{
-    fclose(infile);
-}
+    if (feof(infile))
+    {
+        fclose(infile);
+    }
 
-return 0;
+    return 0;
 }
 
 
 bool find_JPEG(BYTE buffer[])
 {
 // check if JPEG
-if  (buffer[0] == 0xff &&
-    buffer[1] == 0xd8 &&
-    buffer[2]== 0xff &&
-    (buffer[3] & 0xf0) == 0xe0)
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2]== 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
         return true;
     }
