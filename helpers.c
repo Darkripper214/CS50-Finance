74727def202ea41dba4dfc6c1fd90ac9fe06208d
Darkripper214 <Darkripper214@users.noreply.github.com>
2020-02-10 16:44:20 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -26,33 +26,33 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
         {
-            for (int j = 0; j < width; j++)
+            int red = round(image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue * 0.189);
+            int green = round(image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen * 0.686 + image[i][j].rgbtBlue * 0.168);
+            int blue = round(image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen * 0.534 + image[i][j].rgbtBlue * 0.131);
+
+            if (red > 255)
             {
-                int red = round(image[i][j].rgbtRed*0.393 + image[i][j].rgbtGreen*0.769 + image[i][j].rgbtBlue*0.189);
-                int green = round(image[i][j].rgbtRed*0.349 + image[i][j].rgbtGreen*0.686 + image[i][j].rgbtBlue*0.168);
-                int blue = round(image[i][j].rgbtRed*0.272 + image[i][j].rgbtGreen*0.534 + image[i][j].rgbtBlue*0.131);
-
-                if (red > 255)
-                {
-                    red = 255;
-                }
-
-                if (green > 255)
-                {
-                    green = 255;
-                }
-
-                if (blue > 255)
-                {
-                    blue = 255;
-                }
-
-                image[i][j].rgbtBlue = blue;
-                image[i][j].rgbtGreen = green;
-                image[i][j].rgbtRed = red;
+                red = 255;
             }
+
+            if (green > 255)
+            {
+                green = 255;
+            }
+
+            if (blue > 255)
+            {
+                blue = 255;
+            }
+
+            image[i][j].rgbtBlue = blue;
+            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtRed = red;
         }
+    }
 
     return;
 }
@@ -63,15 +63,15 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     //Scan height
     for (int i = 0; i < height; i++)
     {
-        int halfWidth = round((float)1/2 * width);
+        int halfWidth = round((float)1 / 2 * width);
         for (int j = 0; j < width; j++)
         {
 
             while (j < halfWidth)
             {
                 RGBTRIPLE temp = image[i][j];
-                image[i][j] = image[i][width - j-1];
-                image[i][width - j-1] = temp;
+                image[i][j] = image[i][width - j - 1];
+                image[i][width - j - 1] = temp;
                 break;
             }
         }
